0e2506aeb9e815883443dbdc5c8123bf6f097cfb
Deen-2601 <yussufnurudeen@gmail.com>
2023-01-31 21:55:38 +0100

    init

--- tests/FirstTest.spec.js
+++ tests/FirstTest.spec.js
@@ -1,1 +1,10 @@
 const {test, expect} = require('@playwright/test');
+
+test('First Test', async ({page})=> {
+
+   await page.goto('https://opensource-demo.orangehrmlive.com/')
+   await expect(page).toHaveTitle('OrangeHRM')
+
+
+
+})
